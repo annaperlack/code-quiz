67cd646cfb21bf7b264b4dfceddadee254f1c673
Anna Perlack <annaperlack@gmail.com>
2022-11-16 17:35:00 -0500

    added function to delete highscores from ordered list

--- assets/scores.js
+++ assets/scores.js
@@ -1,8 +1,15 @@
 var highScoreList = JSON.parse(localStorage.getItem("highScores")) || [];
+var ol = document.getElementById ("highscores");
 
 for (var i =0; i < highScoreList.length; i++) {
-    var ol = document.getElementById ("highscores");
     var li = document.createElement ("li");
     li.textContent = "player: " + highScoreList[i].initials + "  score: " + highScoreList[i].score;
     ol.append(li);
 }
+
+function clearHighscores() {
+    localStorage.removeItem('highScores');
+    ol.textContent = "";
+  }
+
+document.getElementById('clear').onclick = clearHighscores;
