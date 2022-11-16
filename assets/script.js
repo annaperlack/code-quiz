d3d829a9a285e52925b87aff7644cab3e59cea8f
Anna Perlack <annaperlack@gmail.com>
2022-11-16 16:53:27 -0500

    updated CSS and linked CSS page to highscores page

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,3 @@
-//GIVEN I am taking a code quiz
-//WHEN I click the start button
-//THEN a timer starts and I am presented with a question
-//WHEN I answer a question
-//THEN I am presented with another question
-//WHEN I answer a question incorrectly
-//THEN time is subtracted from the clock
-//WHEN all questions are answered or the timer reaches 0
-//THEN the game is over
-//WHEN the game is over
-//THEN I can save my initials and my score
-
 var questions = [
     {
         title: "Which item is not a javascript value?",
@@ -28,6 +16,7 @@ var questions = [
     }
 ]
 
+//DOM variables 
 var currentQuestionIndex = 0;
 var clock = questions.length * 15;
 var timerId;
